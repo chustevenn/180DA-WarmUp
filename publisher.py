276be68a8556ccc4232476fb4dcd64a9cea81fd2
Steven Chu <schu92620@gmail.com>
2021-10-21 04:42:40 +0100

    Adding RaspberryPi Publisher

---
+++ publisher.py
@@ -0,0 +1,32 @@
+import paho.mqtt.client as mqtt
+import numpy as np
+
+def on_connect(client, userdata, flags, rc):
+    print("Connection returned result: " + str(rc))
+
+def on_disconnect(client, userdata, rc):
+    if rc != 0:
+        print("unexpected disconnect")
+    else:
+        print("expected disconnect")
+
+def on_message(client, userdata, message):
+    print('Received message: "' + str(message.payload) + '" on topic "'
+            + message.topic + '" with QoS ' + str(message.qos))
+
+client = mqtt.Client()
+
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_message = on_message
+
+client.connect_async('mqtt.eclipseprojects.io')
+
+client.loop_start()
+
+words = ['yes', 'no', 'maybe', 'so', 'sad', 'life', 'unfortunate', 'words']
+for word in words:
+    client.publish('ece180d/test', float(np.random.random(1)), qos=1)
+
+client.loop_stop()
+client.disconnect()
